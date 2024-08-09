c578417e561cc58214abc0b7ad049de9d2bab778
Eric <stephen.erich243@gmail.com>
2024-08-09 08:38:02 +0000

    serializer updated

--- api/serializers.py
+++ api/serializers.py
@@ -4,5 +4,5 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['title', 'description', 'price']
+        fields = ['id', 'title', 'description', 'price']
         read_only_fields = ['created_at', 'updated_at']
