c396bdc02a228a0414b40be3c72c227f161fd0d0
Eric <stephen.erich243@gmail.com>
2024-08-09 08:19:11 +0000

    updated

--- api/serializers.py
+++ api/serializers.py
@@ -4,4 +4,5 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ['title', 'description', 'price']
+        read_only_fields = ['created_at', 'updated_at']
--- api/views.py
+++ api/views.py
@@ -2,6 +2,9 @@ from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
 
+
+
 class ProductViewSet(viewsets.ModelViewSet):
+    
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
