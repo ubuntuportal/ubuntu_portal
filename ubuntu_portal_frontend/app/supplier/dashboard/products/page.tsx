d1dc18664d39539d3b5f0f0885fc2e60c0bbe55a
MayDan12 <oluwamayowadan@gmail.com>
2024-08-10 22:19:10 +0100

    added the product table

--- ubuntu_portal_frontend/app/supplier/dashboard/products/page.tsx
+++ ubuntu_portal_frontend/app/supplier/dashboard/products/page.tsx
@@ -1,10 +1,12 @@
 import { AddProduct } from "@/components/supplier/AddProduct";
+import { Tables } from "@/components/supplier/Table";
 import React from "react";
 
 function ProductsPage() {
   return (
     <div>
       <AddProduct />
+      <Tables />
     </div>
   );
 }
