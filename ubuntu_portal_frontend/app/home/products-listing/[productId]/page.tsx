7c3d7c2deab6587b5faa8fc0221c275f890915dc
MayDan12 <oluwamayowadan@gmail.com>
2024-08-26 16:15:27 +0100

    product details

--- ubuntu_portal_frontend/app/home/products-listing/[productId]/page.tsx
+++ ubuntu_portal_frontend/app/home/products-listing/[productId]/page.tsx
@@ -1,16 +1,18 @@
 "use client";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { ReactNode, useState } from "react";
 import { BreadcrumbResponsive } from "@/components/buyer/Breadcrumb";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { CarouselSize } from "@/components/buyer/Carousel";
-// import {
-//   BellIcon,
-//   CogIcon,
-//   SearchIcon,
-//   UserCircleIcon,
-// } from "@heroicons/react/outline";
+import Link from "next/link";
+
+interface TabButtonProps {
+  index: number;
+  activeTab: number;
+  setActiveTab: (index: number) => void;
+  children: ReactNode;
+}
 
 export default function ProductDetails({
   params,
@@ -19,10 +21,10 @@ export default function ProductDetails({
 }) {
   return (
     <div className="bg-gray-100 ">
-      <div className="py-8 px-16 hidden lg:block lg:w-1/4">
+      <div className="py-6 px-16 hidden lg:block lg:w-1/4">
         <BreadcrumbResponsive />
       </div>
-      <h1>Details about {params.productId}</h1>
+      {/* <h1>Details about {params.productId}</h1> */}
       <div className="container px-16  bg-white">
         <div className="flex pt-6">
           <div className="flex-1">
@@ -47,7 +49,7 @@ export default function ProductDetails({
               <Button>Start Order</Button>
               <Button className="bg-[#2DB224]">Add to Cart</Button>
             </div>
-            <div className="grid grid-cols-2 gap-5 text-sm">
+            <div className="grid w-[70%] grid-cols-2 gap-3 text-xs">
               <div>
                 <span></span>
                 <p>Free Shipping on order from $500</p>
@@ -70,7 +72,102 @@ export default function ProductDetails({
         <div className="pl-8 mb-12">
           <CarouselSize />
         </div>
-        <div></div>
+        <div>
+          <Tabs />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Tabs() {
+  const [activeTab, setActiveTab] = useState(0);
+
+  return (
+    <div className="border mb-10">
+      <div className="flex gap-6 justify-center my-6 border-b-2">
+        <TabButton index={0} activeTab={activeTab} setActiveTab={setActiveTab}>
+          Additional information
+        </TabButton>
+        <TabButton index={1} activeTab={activeTab} setActiveTab={setActiveTab}>
+          Specification
+        </TabButton>
+        <TabButton index={2} activeTab={activeTab} setActiveTab={setActiveTab}>
+          Review
+        </TabButton>
+      </div>
+
+      <div className="inspiration-content">
+        {activeTab === 0 && <AdditionalTab />}
+        {/* {activeTab === 1 && <DestinationOutdoorAdventure />}
+        {activeTab === 2 && <MountainCabins />}
+        {activeTab === 3 && <BeachDestinations />}
+        {activeTab === 4 && <PopularDestinations />}
+        {activeTab === 5 && <UniqueStays />} */}
+      </div>
+    </div>
+  );
+}
+
+function TabButton({
+  index,
+  activeTab,
+  setActiveTab,
+  children,
+}: TabButtonProps) {
+  return (
+    <div>
+      <div
+        // href="#"
+        className={`text-lg my-4 cursor-pointer  ${
+          activeTab === index ? "border-b-2 border-[#2DB224] " : ""
+        }`}
+        onClick={() => setActiveTab(index)}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
+
+function AdditionalTab() {
+  return (
+    <div className="flex gap-4 justify-center px-8">
+      <div className="basis-1/2">
+        <h1>Description</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae sed
+          tenetur aliquid earum, eligendi labore illo architecto? Fugiat
+          explicabo alias beatae quas, laborum perferendis! Ab, placeat! Soluta
+          iusto mollitia labore?
+        </p>
+      </div>
+      <div className="flex-1">
+        <h1>Features</h1>
+        <div className="text-sm">
+          <p>
+            <span></span> Free 1 Year Warranty
+          </p>
+
+          <p>
+            <span></span>Free Shipping & Fastest Delivery
+          </p>
+
+          <p>
+            <span></span>100% Money-back Guarantee
+          </p>
+
+          <p>
+            <span></span>24/7 Customer Support
+          </p>
+
+          <p>
+            <span></span>Secure Payment Method
+          </p>
+        </div>
+      </div>
+      <div className="flex-1">
+        <h1>Shipping Information</h1>
       </div>
     </div>
   );
