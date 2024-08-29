b49cc342a6aa37d55074f6f094310d52285bc3a8
HezekiahsDevAndroidStudio <hezekiahs.dev@devlabs.com>
2024-08-29 01:05:13 +0100

    completed status card

--- ubuntu_portal_frontend/components/supplier/StatsCard.tsx
+++ ubuntu_portal_frontend/components/supplier/StatsCard.tsx
@@ -1,4 +1,20 @@
 function StatsCard() {
-  return <div className="bg-white h-1/4 w-auto p-4"> StatsCard</div>;
+  return (
+    <div className="rounded-2xl bg-white p-4 flex-1  min-w-[130px]">
+      <div className="flex">
+        <img
+          src="/Icon_Order.png"
+          alt="Order"
+          className="h-12 w-12 my-4 mr-4"
+        />
+        <div className="my-4">
+          <h1 className="text-2xl font-bold ">75</h1>
+          <h2 className="mt-0 capitalize text-sm font-normal text-gray-500">
+            Total order
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default StatsCard;
