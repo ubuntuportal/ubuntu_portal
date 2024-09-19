ade80054e85a71b1f0f095452468177a20804fd0
Pcosby5 <pcosby50@gmail.com>
2024-09-19 21:21:23 +0000

    tested the models.py inside Product app in tests/test_models.py

--- ubuntu_portal_backend/products/models.py
+++ ubuntu_portal_backend/products/models.py
@@ -4,14 +4,15 @@ from django.contrib.auth import get_user_model
 import uuid
 
 
-
 # Get the user model
 User = get_user_model()
+
 
 class Category(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='subcategories')
+    parent = models.ForeignKey('self', on_delete=models.CASCADE,
+                               null=True, blank=True, related_name='subcategories')
 
     class Meta:
         indexes = [
@@ -23,13 +24,15 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
     description = models.TextField()
     stock = models.PositiveIntegerField(default=0)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name='products', default=1)
+    seller = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='products', default=1)
     category = models.ManyToManyField(Category, related_name='products')
     rating = models.FloatField(default=0.0)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
@@ -45,12 +48,15 @@ class Product(models.Model):
             models.Index(fields=['title', 'description']),
         ]
 
+
 class ProductVariation(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='variations')
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='variations')
     attribute = models.CharField(max_length=255)
     value = models.CharField(max_length=255)
-    price_modifier = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    price_modifier = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, blank=True)
     stock = models.PositiveIntegerField(default=0)
 
     def __str__(self):
