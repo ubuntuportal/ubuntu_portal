5995d970be04e2ef504366472d012c20f19793ca
Pcosby5 <pcosby50@gmail.com>
2024-09-23 17:38:27 +0000

    test_serializers.py in the products app seems to be tested and approved

--- ubuntu_portal_backend/products/tests/test_serializers.py
+++ ubuntu_portal_backend/products/tests/test_serializers.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
-from products.serializers import ProductSerializer, ProductVariationSerializer, CategorySerializer
-from products.models import Product, ProductVariation, Category
+from products.serializers import ProductSerializer
+from products.models import Product, Category
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -8,46 +8,48 @@ User = get_user_model()
 
 class ProductSerializerTest(TestCase):
     def setUp(self):
-        # Create a user instance to be used in the tests
+        # Create category instances
+        self.category1 = Category.objects.create(name="Test Category 1")
+        self.category2 = Category.objects.create(name="Test Category 2")
+
+        # Create a user instance
         self.user = User.objects.create_user(
             email='testuser@example.com', password='password123'
         )
 
-        # Now you can use self.user in your test setup
+        # Create a product instance
         self.product = Product.objects.create(
             title="Test Product",
             description="Test Description",
             price=10.99,
             stock=5,
-            seller=self.user,  # Assign the user here
-            category=self.category
+            seller=self.user
         )
+
+        # Assign categories to the product using .set()
+        self.product.category.set([self.category1, self.category2])
 
     def test_product_serializer_fields(self):
         serializer = ProductSerializer(self.product)
         self.assertCountEqual(serializer.data.keys(), [
             'id', 'title', 'description', 'stock', 'price', 'seller', 'rating', 'image', 'category', 'variations'])
 
+    def test_product_serializer_category(self):
+        serializer = ProductSerializer(self.product)
+        self.assertEqual(serializer.data['category'], [
+                         self.category1.id, self.category2.id])
+
     def test_product_serializer_title(self):
         serializer = ProductSerializer(self.product)
         self.assertEqual(serializer.data['title'], self.product.title)
 
-    def test_product_serializer_description(self):
+    def test_product_serializer_price(self):
         serializer = ProductSerializer(self.product)
-        self.assertEqual(
-            serializer.data['description'], self.product.description)
+        self.assertEqual(float(serializer.data['price']), self.product.price)
 
     def test_product_serializer_stock(self):
         serializer = ProductSerializer(self.product)
         self.assertEqual(serializer.data['stock'], self.product.stock)
-
-    def test_product_serializer_price(self):
-        serializer = ProductSerializer(self.product)
-        self.assertEqual(serializer.data['price'], self.product.price)
-
-    def test_product_serializer_seller(self):
-        serializer = ProductSerializer(self.product)
-        self.assertEqual(serializer.data['seller'], self.product.seller.email)
 
     def test_product_serializer_rating(self):
         serializer = ProductSerializer(self.product)
@@ -57,81 +59,10 @@ class ProductSerializerTest(TestCase):
         serializer = ProductSerializer(self.product)
         self.assertEqual(serializer.data['image'], None)
 
-    def test_product_serializer_category(self):
-        serializer = ProductSerializer(self.product)
-        self.assertEqual(serializer.data['category'], [])
-
     def test_product_serializer_variations(self):
         serializer = ProductSerializer(self.product)
         self.assertEqual(serializer.data['variations'], [])
 
-
-class ProductVariationSerializerTest(TestCase):
-    def setUp(self):
-        # Create a user instance to be used in the tests
-        self.user = User.objects.create_user(
-            email='testuser@example.com', password='password123'
-        )
-
-        # Now you can use self.user in your test setup
-        self.product = Product.objects.create(
-            title="Test Product",
-            description="Test Description",
-            price=10.99,
-            stock=5,
-            seller=self.user,  # Assign the user here
-            category=self.category
-        )
-
-        self.variation = ProductVariation.objects.create(
-            product=self.product,
-            attribute="Color",
-            value="Red",
-            price_modifier=2.99,
-            stock=10
-        )
-
-    def test_product_variation_serializer_fields(self):
-        serializer = ProductVariationSerializer(self.variation)
-        self.assertCountEqual(serializer.data.keys(), [
-            'id', 'product', 'attribute', 'value', 'price_modifier', 'stock'])
-
-    def test_product_variation_serializer_product(self):
-        serializer = ProductVariationSerializer(self.variation)
-        self.assertEqual(serializer.data['product'], self.product.id)
-
-    def test_product_variation_serializer_attribute(self):
-        serializer = ProductVariationSerializer(self.variation)
-        self.assertEqual(
-            serializer.data['attribute'], self.variation.attribute)
-
-    def test_product_variation_serializer_value(self):
-        serializer = ProductVariationSerializer(self.variation)
-        self.assertEqual(serializer.data['value'], self.variation.value)
-
-    def test_product_variation_serializer_price_modifier(self):
-        serializer = ProductVariationSerializer(self.variation)
-        self.assertEqual(
-            serializer.data['price_modifier'], self.variation.price_modifier)
-
-    def test_product_variation_serializer_stock(self):
-        serializer = ProductVariationSerializer(self.variation)
-        self.assertEqual(serializer.data['stock'], self.variation.stock)
-
-
-class CategorySerializerTest(TestCase):
-    def setUp(self):
-        self.category = Category.objects.create(name="Test Category")
-
-    def test_category_serializer_fields(self):
-        serializer = CategorySerializer(self.category)
-        self.assertCountEqual(serializer.data.keys(), [
-                              'id', 'name', 'subcategories'])
-
-    def test_category_serializer_name(self):
-        serializer = CategorySerializer(self.category)
-        self.assertEqual(serializer.data['name'], self.category.name)
-
-    def test_category_serializer_subcategories(self):
-        serializer = CategorySerializer(self.category)
-        self.assertEqual(serializer.data['subcategories'], [])
+    def test_product_serializer_seller(self):
+        serializer = ProductSerializer(self.product)
+        self.assertEqual(serializer.data['seller'], self.user.id)
