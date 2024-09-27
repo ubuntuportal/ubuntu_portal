eaf10615c70bcb2635b756ac1cb1f1400676c9ca
HezekiahsDevAndroidStudio <hezekiahs.dev@devlabs.com>
2024-09-27 17:08:02 +0100

    Still working on responsiveness

--- ubuntu_portal_frontend/app/supplier/dashboard/layout.tsx
+++ ubuntu_portal_frontend/app/supplier/dashboard/layout.tsx
@@ -17,7 +17,7 @@ export default function SupplierLayout({
     <main className="bg-gray-50">
       <div className="h-screen flex">
         <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] overflow-scroll flex-1 flex-col">
+        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] overflow-scroll flex-1 flex-col mb-2">
           <Header toggleSidebar={toggleSidebar} />
           <section>{children}</section>
         </div>
